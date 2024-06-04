94e456e60dfe6dc07434a60fba64083651e2ff20
Surikat12 <Surikat12@yandex.ru>
2024-06-04 10:25:06 +0300

    Added NotificationRepository Added interface NotificationRepository and its implementation NotificationRepositoryImpl

---
+++ notify-service-service/src/main/java/com/surikat/documents/notifyservice/service/repository/NotificationRepository.java
@@ -0,0 +1,8 @@
+package com.surikat.documents.notifyservice.service.repository;
+
+import com.surikat.documents.notifyservice.common.NotificationModel;
+
+public interface NotificationRepository {
+
+    void insert(NotificationModel notification);
+}
