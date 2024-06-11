3ee1fa0576f576e8c4ef1eb83b9cb6a0c43fd899
Surikat12 <Surikat12@yandex.ru>
2024-06-11 12:25:36 +0300

    Added app description in swagger Added app title, description and version in swagger

--- notify-service-service/src/main/java/com/surikat/documents/notifyservice/service/NotifyServiceApp.java
+++ notify-service-service/src/main/java/com/surikat/documents/notifyservice/service/NotifyServiceApp.java
@@ -1,5 +1,7 @@
 package com.surikat.documents.notifyservice.service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication()
 @ComponentScan({"com.surikat"})
+@OpenAPIDefinition(info = @Info(title = "${info.app.name}", description = "${info.app.description}", version = "${info.app.version}"))
 @EnableTransactionManagement
 public class NotifyServiceApp {
     public static void main(String[] args) {
