850d1e54079290a3d652843124b6094d2ae9ea1a
Surikat12 <Surikat12@yandex.ru>
2024-06-11 12:18:39 +0300

    Added transactions Added transaction in NotifyService

--- notify-service-service/src/main/java/com/surikat/documents/notifyservice/service/NotifyServiceApp.java
+++ notify-service-service/src/main/java/com/surikat/documents/notifyservice/service/NotifyServiceApp.java
@@ -3,9 +3,11 @@ package com.surikat.documents.notifyservice.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication()
 @ComponentScan({"com.surikat"})
+@EnableTransactionManagement
 public class NotifyServiceApp {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(NotifyServiceApp.class);
